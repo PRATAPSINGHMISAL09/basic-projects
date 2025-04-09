ae028d2a7b0fbd60134d99a4541471ee297f3dbc
Pratap <pratapmisalngu@gmail.com>
2025-04-09 17:35:00 +0530

    tic tac tie game

--- my-auth-app/src/firebase.js
+++ my-auth-app/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAxuLZZAPl6vFk9S3D97eFQQI-5KdCURTs",
-  authDomain: "double-auth-1e101.firebaseapp.com",
-  projectId: "double-auth-1e101",
-  storageBucket: "double-auth-1e101.firebasestorage.app",
-  messagingSenderId: "511512218648",
-  appId: "1:511512218648:web:e29ab9d34b31959e43476d",
-  measurementId: "G-GD5G1P4T1F"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "" ,
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
